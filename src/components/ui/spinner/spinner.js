9765937880a9802a02a3eac254845690762f2294
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2022-03-19 11:06:47 +0300

    spinner add

--- src/components/ui/spinner/spinner.js
+++ src/components/ui/spinner/spinner.js
@@ -3,7 +3,7 @@ import './spinner.css'
 const Spinner = () => {
     return(
         <div className='spinner'>
-          <div class="loadingio-spinner-spinner-vh4py2ukxyh"><div class="ldio-pqe7xquf8d">
+          <div className="loadingio-spinner-spinner-vh4py2ukxyh"><div className="ldio-pqe7xquf8d">
           <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
           </div></div>
         </div>
